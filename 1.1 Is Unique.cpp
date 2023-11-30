a67aa78a62de3ed8e0be3aaa02a28d25531fa4d4
PRIYAM AGARWAL <95881431+PriyamAg001@users.noreply.github.com>
2023-11-30 15:22:59 +0530

    Add files via upload

---
+++ 1.1 Is Unique.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+bool isUnique(string str){
+    for(int i=0; i<str.length()-1; i++){
+        for(int j=i+1; j<str.length(); j++){
+            if(str[i] == ' ' || str[j] == ' '){
+                continue;
+            }
+            if(str[i] == str[j]){
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+int main(){
+    cout << "Enter the String: ";
+    string str;
+    getline(cin, str);
+
+    if(isUnique(str)){
+        cout << "The String '" << str << "' has all unique characters";
+    }
+    else{
+        cout << "The String '" << str << "' has duplicate characters";
+    }
+    return 0;
+}
